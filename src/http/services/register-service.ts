7478979df52483e9fea684e7174466a5a4dd50b5
Gabriel Medrado <medradoogabriel@gmail.com>
2025-01-13 15:41:31 -0300

    fix: fix lint

--- src/http/services/register-service.ts
+++ src/http/services/register-service.ts
@@ -4,26 +4,20 @@ import { IUsersRepository } from "../repositories/users-repository-interface";
 interface registerUseCaseRequest {
   name: string;
   email: string;
-  password: string; 
-}  
+  password: string;
+}
 
 export class RegisterUseCase {
-  constructor(
-    private usersRepository: IUsersRepository
-  ) {}
-  async execute ({
-    name,
-    email,
-    password,
-  }: registerUseCaseRequest) {
+  constructor(private usersRepository: IUsersRepository) {}
+  async execute({ name, email, password }: registerUseCaseRequest) {
     const password_hash = await hash(password, 6);
 
     const userWithTheSameEmail = await this.usersRepository.findByEmail(email);
-  
+
     if (userWithTheSameEmail) {
       throw new Error("E-mail already exists");
     }
-  
+
     await this.usersRepository.create({ name, email, password_hash });
   }
 }
