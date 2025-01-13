56bdb9ae547629b0f02cc38b996dc6608ff59a90
Gabriel Medrado <medradoogabriel@gmail.com>
2025-01-13 16:46:18 -0300

    feat: (register-service): add error treatment

--- src/http/services/register-service.ts
+++ src/http/services/register-service.ts
@@ -1,5 +1,6 @@
 import { hash } from "bcryptjs";
 import { IUsersRepository } from "../repositories/users-repository-interface";
+import { UserAlreadyExistsError } from "./errors/user-alredy-exists-error";
 
 interface registerUseCaseRequest {
   name: string;
@@ -15,7 +16,7 @@ export class RegisterUseCase {
     const userWithTheSameEmail = await this.usersRepository.findByEmail(email);
 
     if (userWithTheSameEmail) {
-      throw new Error("E-mail already exists");
+      throw new UserAlreadyExistsError();
     }
 
     await this.usersRepository.create({ name, email, password_hash });
