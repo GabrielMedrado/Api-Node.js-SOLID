164ae5c13cfff41095c0a2fca5cba95ef7c66803
Gabriel Medrado <medradoogabriel@gmail.com>
2025-04-05 13:32:08 -0300

    feat: add check-in history endpoint

---
+++ src/http/services/fetch-user-check-ins-history-service.spec.ts
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { beforeEach, describe, expect, it, afterEach, vi } from "vitest";
+import { InMemoryCheckInsRepository } from "../repositories/in-memory/in-memory-check-ins-repository";
+import { CheckInService } from "./check-in-service";
+import { FetchUserCheckInsHistoryService } from "./fetch-user-check-ins-history-service";
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let sut: FetchUserCheckInsHistoryService;
+
+describe("Fetch user check-in history Service", () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    sut = new FetchUserCheckInsHistoryService(checkInsRepository);
+  });
+
+  it("should be able to fetch check-in history", async () => {
+    await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    await checkInsRepository.create({
+      gym_id: "gym-02",
+      user_id: "user-01",
+    });
+
+    const { checkIns } = await sut.execute({
+      userId: "user-01",
+      page: 1,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual([
+      expect.objectContaining({
+        gym_id: "gym-01",
+      }),
+      expect.objectContaining({
+        gym_id: "gym-02",
+      }),
+    ]);
+  });
+  it("should be able to fetch paginated check-in history", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await checkInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: "user-01",
+      });
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: "user-01",
+      page: 2,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect.objectContaining({
+      gym_id: "gym-21",
+    });
+    expect.objectContaining({
+      gym_id: "gym-22",
+    });
+  });
+});
