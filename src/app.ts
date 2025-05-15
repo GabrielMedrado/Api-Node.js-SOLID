18818ff810e6f1b3a4f2eced61461bfbfb7def96
Gabriel Medrado <medradoogabriel@gmail.com>
2025-05-15 17:20:30 -0300

    feat: implements JWT

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
+
 import { appRoutes } from "./http/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
 
 export const app = fastify();
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: "10m",
+  },
+});
 app.register(appRoutes);
 
 app.setErrorHandler((error, _, reply) => {
--- src/http/controllers/profile-controller.ts
+++ src/http/controllers/profile-controller.ts
@@ -4,5 +4,7 @@ export async function profileController(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
+  await request.jwtVerify();
+
   return reply.status(200).send();
 }
