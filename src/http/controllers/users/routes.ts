00e33d532ec94028a89eb5f5351a9130f8e8e518
Gabriel Medrado <medradoogabriel@gmail.com>
2025-08-22 11:13:33 -0300

    feat() implements refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,10 +5,13 @@ import { verifyJWT } from "../../middlewares/verify-jwt";
 import { registerController } from "./register-controller";
 import { authenticateController } from "./authenticate-controller";
 import { profileController } from "./profile-controller";
+import { refreshTokenController } from "./refresh-token-controller";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", registerController);
   app.post("/sessions", authenticateController);
+
+  app.patch("/token/refresh", refreshTokenController);
 
   //* Authenticated*/
   app.get("/me", { onRequest: [verifyJWT] }, profileController);
